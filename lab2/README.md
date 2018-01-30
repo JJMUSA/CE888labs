7f3ccc76bd184005a6cdaaf74b1f2564738a2385
mlvm2 <thejabirmusa@gmail.com>
2018-01-30 13:42:33 +0000

    added .png files

--- lab2/README.md
+++ lab2/README.md
@@ -3,23 +3,23 @@
 
 Here is a scatter showing the MPG of the different Fleet of cars
 #Scatter Plot of current fleet
-![logo](./currentFleet_scattr.pdf?raw=true)
+![logo](./currentFleet_scattr.png?raw=true)
 
 #Scatter Plot of new fleet
-![logo](./newFleet_scatter.pdf?raw=true)
+![logo](./newFleet_scatter.png?raw=true)
 
 Here is a histogram of the fleets MPG
 #Histogram of current fleet
-![logo](./currentFleet_histogram.pdf?raw=true)
+![logo](./currentFleet_histogram.png?raw=true)
 
 #Histogram of new fleet
-![logo](./new_fleet_histogram.pdf?raw=true)
+![logo](./new_fleet_histogram.png?raw=true)
 Here is a standard deviation of the different fleet of car
 #Bootstrapping is done to find the lower and upper bound of the standard deviation of current fleet 
-![logo](./currentFleet_bootstrap.pdf?raw=true)
+![logo](./currentFleet_bootstrap.png?raw=true)
 
 #Bootstrapping is done to find the lower and upper bound of the standard deviation of  
-![logo](./currentFleet_bootstrap.pdf?raw=true)
+![logo](./currentFleet_bootstrap.png?raw=true)
 
 
 
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -22,12 +22,12 @@ newFleet.insert(loc=0,column='ID',value=cols)
 
 sns.set(color_codes=True)
 
-'''plotting scatter_plot
+#plotting scatter_plot
 currentFleet_scatter=sns.lmplot(currentFleet.columns[0],currentFleet.columns[1],data=currentFleet,fit_reg=False)
-currentFleet_scatter.savefig("currentFleet_scatter.pdf",bbox_inches='tight')
+currentFleet_scatter.savefig("currentFleet_scatter.png",bbox_inches='tight')
 plt.clf()
 newFleet_scatter=sns.lmplot(newFleet.columns[0],newFleet.columns[1],data=newFleet,fit_reg=False)
-newFleet_scatter.savefig('newFleet_scatter.pdf',bbox_inches='tight')
+newFleet_scatter.savefig('newFleet_scatter.png',bbox_inches='tight')
 
 #plotting histogram
 plt.clf()
@@ -36,12 +36,11 @@ axes=plt.gca()
 axes.set_xlabel("Current Fleet")
 axes.set_ylabel('Probability')
 
-currentFleet_histogram.savefig('currentFleet_histogram.pdf',bbox_inches='tight')
+currentFleet_histogram.savefig('currentFleet_histogram.png',bbox_inches='tight')
 newFleet_histogram=sns.distplot(newFleet.T[1],bins=7,kde=False,rug=True).get_figure()
 axes.set_xlabel("New Fleet")
 axes.set_ylabel('probability')
-newFleet_histogram.savefig('new_fleet_histogram.pdf',bbox_inches='tight')
-'''
+newFleet_histogram.savefig('new_fleet_histogram.png',bbox_inches='tight')
 
 
 #calculating standard deviation
@@ -85,10 +84,10 @@ for r,i in enumerate(range(100,10000,1000)):
 currentFleet_bootstrap=pd.DataFrame(currentFleet_bootstrap,columns=["iterations",'mean','value'])
 newFleet_bootstrap=pd.DataFrame(newFleet_bootstrap,columns=['iterations','mean','value'])
 current_boot=sns.lmplot(currentFleet_bootstrap.columns[0],currentFleet_bootstrap.columns[1],data=currentFleet_bootstrap,hue='value')
-current_boot.savefig("currentFleet_bootstrap.pdf",bbox_inches='tight')
+current_boot.savefig("currentFleet_bootstrap.png",bbox_inches='tight')
 
 newFleet_boot=sns.lmplot(newFleet_bootstrap.columns[0],newFleet_bootstrap.columns[1],data=newFleet_bootstrap,hue='value')
-newFleet_boot.savefig("newFleet_bootstrap.pdf",bbox_inches='tight')
+newFleet_boot.savefig("newFleet_bootstrap.png",bbox_inches='tight')
 
 plt.show()
 
